481b9b1213856056ad9714a61b1c2a6dcb456a12
garvkhurana <garvkhurana1234567@gmail.com>
2025-02-25 02:00:59 +0530

    utisl part done

--- src/logging.py
+++ src/logging.py
@@ -12,6 +12,7 @@ log_file_path=os.path.join(log_path,log_file)
 logging.basicConfig(filename=log_file_path,level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s')
 
 
+logger=logging.getLogger('fine_tuned_logger')
 
 if __name__=='__main__':
     logging.info('This is a test log message')
